ef988938e1f28877c18547b7ab283f124e742978
juanECR <rjuanelias2@gmail.com>
2025-07-04 00:08:48 -0500

    commit1

---
+++ paginaDPW/src/App.jsx
@@ -0,0 +1,12 @@
+import Header from './Header.jsx'
+import './App.css'
+function App() {
+
+  return (
+    <>
+    <Header></Header>
+    </>
+  )
+}
+
+export default App
---
+++ paginaDPW/src/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+const react = ReactDOM.createRoot(document.getElementById('root'))
+
+react.render(
+<>
+<App></App>
+</>
+)
